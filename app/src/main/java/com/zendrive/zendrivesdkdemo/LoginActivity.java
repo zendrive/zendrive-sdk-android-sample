2461b108952a81b2d2b67ec47a57bf3f9ba808b2
Deepanshu Gupta <deepanshu@zendrive.com>
2019-02-06 17:38:38 +0530

    Request overlay permission for Phone Interaction
    Zendrive SDK needs permission to draw above other apps to detect phone
    screen interactions. This permission is granted automatically if the app
    is installed from Play Store. However, when apps are installed via adb
    or some other store, the permission needs to be explicitly requested.
    
    Change-Id: I38f46bcfce8bcbe2a9d9b618d77eb75de91a5100

--- app/src/main/java/com/zendrive/zendrivesdkdemo/LoginActivity.java
+++ app/src/main/java/com/zendrive/zendrivesdkdemo/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.zendrive.zendrivesdkdemo;
 
 import android.app.Activity;
+import android.app.AppOpsManager;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,24 +20,86 @@ import com.zendrive.zendrivesdkdemo.databinding.ActivityLoginBinding;
  */
 public class LoginActivity extends Activity implements View.OnClickListener {
 
-    ActivityLoginBinding binding;
+    private static final String TAG = "LoginActivity";
+    private static final int DRAW_PERMISSION_REQUEST_CODE = 1;
+
+    private AppOpsManager.OnOpChangedListener onOpChangedListener = null;
+    private boolean hasOverlayPermission;
+    private ActivityLoginBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         binding.setClickHandler(this);
+        hasOverlayPermission = !overlayPermissionNeeded() || Settings.canDrawOverlays(this);
+        updateOverlayPermissionButtonVisibility();
     }
 
     public void onClick(View v) {
-        // Display DashboardFragment
-        String driverId = binding.editText.getText().toString();
-        if (!Zendrive.isValidInputParameter(driverId)) {
-            Toast.makeText(this, "Enter valid driver id", Toast.LENGTH_SHORT).show();
-            return;
+        if (v == binding.loginButton) {
+            // Display DashboardFragment
+            String driverId = binding.editText.getText().toString();
+            if (!Zendrive.isValidInputParameter(driverId)) {
+                Toast.makeText(this, "Enter valid driver id", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            SharedPreferenceManager.setDriverId(LoginActivity.this, driverId);
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else if (v == binding.overlayPermissionButton) {
+            if (overlayPermissionNeeded()) {
+                onOpChangedListener = new AppOpsManager.OnOpChangedListener() {
+                    @Override
+                    public void onOpChanged(String op, String packageName) {
+                        if (AppOpsManager.OPSTR_SYSTEM_ALERT_WINDOW.equals(op) &&
+                                getPackageName().equals(packageName)) {
+                            hasOverlayPermission = !hasOverlayPermission;
+                        }
+                    }
+                };
+                AppOpsManager appOpsManager = getSystemService(AppOpsManager.class);
+                // Do not specify the package name here. That will result in duplicate calls to
+                // onOpChanged - one for when package name matches and one for when mode matches
+                appOpsManager.startWatchingMode(AppOpsManager.OPSTR_SYSTEM_ALERT_WINDOW,
+                        null, onOpChangedListener);
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, DRAW_PERMISSION_REQUEST_CODE);
+            } else {
+                Log.d(TAG, "overlayPermission button clicked when it should have been invisible");
+            }
         }
-        SharedPreferenceManager.setDriverId(LoginActivity.this, driverId);
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == DRAW_PERMISSION_REQUEST_CODE) {
+            if (overlayPermissionNeeded()) {
+                // Cannot use Settings.canDrawOverlays() here directly because of
+                // https://issuetracker.google.com/issues/66072795
+                // Hence we must use an onOpChangedListener and monitor changes ourselves
+                AppOpsManager appOpsManager = getSystemService(AppOpsManager.class);
+                appOpsManager.stopWatchingMode(onOpChangedListener);
+                if (!hasOverlayPermission) {
+                    Toast.makeText(this,
+                            "Need draw permission to generate phone screen interaction events",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            updateOverlayPermissionButtonVisibility();
+        }
+    }
+
+    private void updateOverlayPermissionButtonVisibility() {
+        if (hasOverlayPermission) {
+            binding.overlayPermissionButton.setVisibility(View.GONE);
+        } else {
+            binding.overlayPermissionButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private boolean overlayPermissionNeeded() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
     }
 }
