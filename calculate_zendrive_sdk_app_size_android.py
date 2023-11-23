3a636e6946ee4b6c0d9d1b55f2690ee53f4327f5
Ranjit Prasad <ranjit@zendrive.com>
2023-11-23 20:54:52 +0530

    QA-4073 : Automate sample app install size for Android
    Change-Id: Ic15c46d67bb1cb3171d932827d3153675dab5fe7

---
+++ calculate_zendrive_sdk_app_size_android.py
@@ -0,0 +1,168 @@
+import argparse
+import fileinput
+import re
+import subprocess
+import os
+
+git_root = os.environ.get("GIT_ROOT")
+sdk_android_sample_project_path = os.path.join(git_root, "../sdk-android-sample/")
+
+
+# Check Java version is set as 11
+def check_java_version():
+    # Run the 'java -version' command and capture the output
+    try:
+        java_version_output = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT, universal_newlines=True)
+    except subprocess.CalledProcessError as e:
+        java_version_output = e.output
+
+    # Check if the output contains the desired version
+    if "version \"11" in java_version_output:
+        print("Java version 11 is set.")
+    else:
+        print("Java version is not set to 11 or Java may not be installed.")
+
+
+def build_and_bundle_android_project():
+    print("\tClean android project")
+    # Clean the project
+    clean_command = "./gradlew clean"
+    subprocess.run(clean_command.split())
+    print("\tClean successful")
+
+    print("\tBuild android project")
+    # Build the project
+    build_command = "./gradlew assembleDebug"
+    subprocess.run(build_command.split())
+    print("\tBuild successful")
+
+    print("\tBundle android project")
+    # Bundle the project
+    build_command = "./gradlew bundle"
+    subprocess.run(build_command.split())
+    print("\tBundle successful")
+
+
+def get_min_max_sample_app_size(release_type):
+    os.chdir(sdk_android_sample_project_path)
+    os.chdir(f"app/build/outputs/bundle/{release_type}")
+    home_directory = os.path.expanduser("~")
+
+    print("\tBuild apk using bundle tool")
+    # Build the project
+    build_command = f"java -jar {home_directory}/bundletool-all-1.15.4.jar build-apks --bundle=app-{release_type}.aab --output=my_app.apks"
+    subprocess.run(build_command.split())
+    print("\tApk builded successfully")
+
+    # Build the project
+    build_command = f"java -jar {home_directory}/bundletool-all-1.15.4.jar get-size total --apks=my_app.apks"
+    completed_process = subprocess.run(build_command.split(), stdout=subprocess.PIPE)
+    process_output = completed_process.stdout.decode('utf-8')
+    # Split the lines of the output
+    lines = process_output.strip().split('\n')
+    # Split the header and data
+    min_app_size, max_app_size = lines[1].split(",")
+    return min_app_size, max_app_size
+
+
+# Get sample app size
+def get_release_debug_min_max_sample_app_size():
+    build_and_bundle_android_project()
+    release_min_app_size, release_max_app_size = get_min_max_sample_app_size("release")
+    debug_min_app_size, debug_max_app_size = get_min_max_sample_app_size("debug")
+    app_size = {
+        "release_min": int(release_min_app_size)/1000000,
+        "release_max": int(release_max_app_size)/1000000,
+        "debug_min": int(debug_min_app_size)/1000000,
+        "debug_max": int(debug_max_app_size)/1000000
+    }
+    return app_size
+
+
+def update_build_gradle_variable(file_path, variable_name, new_value):
+    # Change the working directory to the root directory of your Android project
+    os.chdir(sdk_android_sample_project_path)
+    updated = False
+    # Regular expression pattern to match the variable assignment
+    pattern = re.compile(rf'\s*def {variable_name}\s*=\s*[\'"].*?[\'"]')
+
+    # Iterate through each line in the file
+    for line in fileinput.input(file_path, inplace=True):
+        # Check if the line contains the variable assignment
+        if re.match(pattern, line):
+            # Update the variable value
+            line = f'    def {variable_name} = \'{new_value}\'\n'
+            updated = True
+        # Output the line (with modifications, if any)
+        print(line, end='')
+
+    # Check if the variable was updated
+    if updated:
+        print(f"Updated '{variable_name}' to '{new_value}' in {file_path}")
+    else:
+        print(f"Variable '{variable_name}' not found in {file_path}")
+
+
+def update_dependency_name(file_path, old_dependency_name, new_dependency_name):
+    os.chdir(sdk_android_sample_project_path)
+    updated = False
+    # Regular expression pattern to match the dependency with the specified name
+    pattern = re.compile(rf'implementation\s+"[^"]*:{old_dependency_name}:\$[a-zA-Z_][a-zA-Z0-9_]*"')
+
+    # Iterate through each line in the file
+    for line in fileinput.input(file_path, inplace=True):
+        # Check if the line contains the dependency with the specified name
+        if re.search(pattern, line):
+            # Update the dependency name
+            line = re.sub(rf'(:{old_dependency_name}:\$[a-zA-Z_][a-zA-Z0-9_]*)', f':{new_dependency_name}:$zendrive_sdk_version', line)
+            updated = True
+        # Output the line (with modifications, if any)
+        print(line, end='')
+
+    # Check if the dependency was updated
+    if updated:
+        print(f"Updated dependency name '{old_dependency_name}' to '{new_dependency_name}' in {file_path}")
+    else:
+        print(f"Dependency '{old_dependency_name}' not found in {file_path}")
+
+
+def main(args):
+    sample_app_without_zendrive_sdk = {
+        "release_min": 0.64,
+        "release_max": 0.67,
+        "debug_min": 1.69,
+        "debug_max": 1.72
+    }
+
+    check_java_version()
+    build_file = 'app/build.gradle'
+
+    # GMS Build
+    update_build_gradle_variable(build_file, "zendrive_sdk_version", args.zendrive_sdk_version)
+    sample_app_with_zendrive_sdk_gms = get_release_debug_min_max_sample_app_size()
+
+    # HMS Build
+    update_dependency_name(build_file, "ZendriveSDK", "ZendriveSDK-HMS")
+    sample_app_with_zendrive_sdk_hms = get_release_debug_min_max_sample_app_size()
+
+    print("\n")
+    for key, value in sample_app_with_zendrive_sdk_gms.items():
+        print(f"GMS {key} value : {value:.2f}")
+
+    print("\n")
+    for key, value in sample_app_with_zendrive_sdk_gms.items():
+        print(f"GMS size increase due to {key} value : {(value - sample_app_without_zendrive_sdk[key]):.2f}")
+
+    print("\n")
+    for key, value in sample_app_with_zendrive_sdk_hms.items():
+        print(f"HMS {key} value : {value:.2f}")
+
+    print("\n")
+    for key, value in sample_app_with_zendrive_sdk_hms.items():
+        print(f"HMS size increase due to {key} value : {(value - sample_app_without_zendrive_sdk[key]):.2f}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--zendrive_sdk_version', dest='zendrive_sdk_version', required=True, help="""Version of zendrive_sdk""")
+    main(args=parser.parse_args())
